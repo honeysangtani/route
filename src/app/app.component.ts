de6fd47960ba3a6c3ec9a4da43a3d18f86b92917
hemaxi <hemaxi.lade@neosofttech.com>
2020-02-14 15:39:19 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { slideInAnimation } from './animation';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [ slideInAnimation ]
 })
 export class AppComponent {
   title = 'angular-routing';
+  navbarOpen = false;
+
+  toggleNavbar() {
+    this.navbarOpen = !this.navbarOpen;
+  }
+
+  getAnimationData(outlet: RouterOutlet) {
+    return outlet && outlet.activatedRouteData && outlet.activatedRouteData.animation;
+  }
+
 }
---
+++ src/app/articles/articles/articles.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-articles',
+  templateUrl: './articles.component.html',
+  styleUrls: ['./articles.component.css']
+})
+export class ArticlesComponent implements OnInit {
+
+  article = {
+    id: 100,
+    title: 'How to make router & navigation in Angular 8',
+    author: 'Didin J.',
+    description: 'A complete tutorial about creating router and navigation in the Angular 8 Web Application'
+  };
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  gotoDetails(articleId: any) {
+    this.router.navigate(['/article/', articleId]);
+  }
+
+}
